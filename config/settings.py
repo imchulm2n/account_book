dca09d77ca7669c70211fe7e2e793d53120fee77
imchulmin <110074127+imchulmin@users.noreply.github.com>
2023-04-26 11:45:24 +0900

    앱 등록

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local apps
+    'accounts',
+    # third apps
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
